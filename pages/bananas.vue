d9bab07c3d77a28c614e319e79e82b6396744d26
Jonas L <joaohccl@hotmail.com>
2020-01-07 17:18:28 +0900

    ok gitkraken

---
+++ pages/bananas.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="main">
+    <h1>
+      bananas
+    </h1>
+    <img v-bind:src="imgSrc" alt="oops i did it again">
+    <div>
+      {{ coiso }}?
+      {{ coiso }}!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Bananas',
+  // TODO https://vuejs.org/v2/guide/computed.html ver se isto dp funciona com estas coizinhas
+  data () {
+    return {
+      coiso: 'sim',
+      imgSrc: 'images/banana.gif'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #main {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: table;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+
+</style>
